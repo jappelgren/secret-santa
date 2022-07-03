79bebcedd6ab515f0d27092b76d149aa836d7bc0
jappelgren <justinappelgren@gmail.com>
2022-07-03 16:19:39 -0500

    moved cursed class into component that uses it

--- components/UserTable/UserTable.tsx
+++ components/UserTable/UserTable.tsx
@@ -4,7 +4,35 @@ import { useEffect, useState } from 'react';
 import { IUserData } from '../../models';
 import Checked from '/public/images/checked.svg';
 import Unchecked from '/public/images/unchecked.svg';
-import UserDataClass  from '../../models/UserDataClass';
+
+class UserDataClass {
+  id;
+  name;
+  email;
+  idea1;
+  idea2;
+  idea3;
+
+  constructor(
+    id: string,
+    name: string,
+    email: string | null,
+    idea1: string | null,
+    idea2: string | null,
+    idea3: string | null
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.idea1 = idea1;
+    this.idea2 = idea2;
+    this.idea3 = idea3;
+  }
+
+  giftIdeasComplete() {
+    return this.idea1 && this.idea2 && this.idea3;
+  }
+}
 
 const UserTable: NextPage = () => {
   const [userData, setUserData] = useState<UserDataClass[]>([]);
